a86ab23bd25937d7757c9573b9fb0e0bf908713e
Odohi David <odohi.david@gmail.com>
2019-11-22 12:45:57 +0100

    add more streams examples

--- src/app/Zipper.java
+++ src/app/Zipper.java
@@ -1,8 +1,12 @@
 package app;
 
 import java.io.ByteArrayInputStream;
+import java.io.CharArrayReader;
+import java.io.InputStream;
 import java.nio.file.FileSystem;
+import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,17 +14,55 @@ import java.util.Map;
 public class Zipper {
     public static void main(String[] args) {
         // Startup();
-        String name = "data.zip";
-        FileHandlers fh = new FileHandlers();
         // fh.PrintByChar();
         // String fc = fh.ReadFromFile("file.txt");
         // System.out.println(fc);
+        
+        // INPUT STREAMS
+        byte[] b = {6, 7, 8, 9, 10, 11, 12, 13, 14, 127, 'a'};
+        // InputStream is = new ByteArrayInputStream( "myString".getBytes() );
+        InputStream is = new ByteArrayInputStream( b );
+        int v;
+        try {
+            while ((v = is.read()) >= 0) {
+                System.out.print((char)v);
+                System.out.print(" : ");
+                System.out.print(v);
+                System.out.println();
+            }
+            System.out.println("\n------------------------------");
+            is.close();
+        } catch(Exception e) {
+            System.out.print(e.getMessage());
+        }
+        char[] cc = {'A', 'B', 'C', '\n'};
+        CharArrayReader r = new CharArrayReader(cc);
+        int c;
+        try {
+            while ((c = r.read()) >= 0) {
+                System.out.print(c);
+                System.out.print(" : ");
+                System.out.print((char)c);
+                System.out.println();
+            }
+            System.out.println("\n------------------------------");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        
+        // InputStream in = Files.newInputStream(Paths.get("file.txt"), Files.)
+
+        String name = "data.zip";
+        FileHandlers fh = new FileHandlers();
         try (FileSystem fs = fh.createZip(Paths.get(name))) {
             for (String f : args) {
                 FileHandlers.copyToZip(fs, f);
                 System.out.println("added '" + f + "' to the archive!");
             }
             System.out.println("Your archive is ready : " + name);
+            Path p = Paths.get(fs.toString());
+            System.out.println(p.toUri().toString());
         } catch(Exception e) {
             System.out.println("Exception : " + e.getMessage());
         }
@@ -55,5 +97,12 @@ public class Zipper {
     }
 }
 
-// jar -cvf zipper.jar app/*.class
-// java -cp ./src/zipper.jar app.Zipper file.txt
+// from src
+// jar -cvf ../zipper.jar app/*.class
+// java -cp ./zipper.jar app.Zipper "file.txt" "file - Copy.txt" "file - Copy (3).txt" "file - Copy (2).txt"
+
+// Bug Tracker
+// Conference scheduling app
+    // Hold different sessions
+    // Allow speakers to be assigned
+    // basically manage the conference topics and 
