2d37b3731434930f0f97d37b2339c81554dc63b0
Odohi David <odohi.david@gmail.com>
2019-11-22 03:38:08 +0100

    add some comment refactor and add type definition for the hashmap

--- src/app/Zipper.java
+++ src/app/Zipper.java
@@ -5,17 +5,16 @@ import java.nio.file.FileSystem;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
-//import java.util.logging.FileHandler;
 
 
 public class Zipper {
     public static void main(String[] args) {
-//        Startup();
+        // Startup();
         String name = "data.zip";
         FileHandlers fh = new FileHandlers();
-//        fh.PrintByChar();
-//        String fc = fh.ReadFromFile("file.txt");
-//        System.out.println(fc);
+        // fh.PrintByChar();
+        // String fc = fh.ReadFromFile("file.txt");
+        // System.out.println(fc);
         try (FileSystem fs = fh.createZip(Paths.get(name))) {
             for (String f : args) {
                 FileHandlers.copyToZip(fs, f);
@@ -23,20 +22,20 @@ public class Zipper {
             }
             System.out.println("Your archive is ready : " + name);
         } catch(Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println("Exception : " + e.getMessage());
         }
     }
 
-//    Some testings
+    // Some testings
     public static void Startup() {
         int c = 123;
         byte b = (byte) c;
         char a = 'a';
-        Map<Integer, String> m = new HashMap<>(){{
+        Map<Integer, String> m = new HashMap<Integer, String>(){{
             put(1, "value");
             put(2, "Value2");
         }};
-//        running anonymous threads
+        // running anonymous threads
         m.forEach((k, v) -> {
             Runnable r = new Runnable() {
                 public void run() {
@@ -55,3 +54,6 @@ public class Zipper {
         System.out.println(a);
     }
 }
+
+// jar -cvf zipper.jar app/*.class
+// java -cp ./src/zipper.jar app.Zipper file.txt
